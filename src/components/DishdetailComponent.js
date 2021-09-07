14d468ab5980a62d814d72f2ea9d984f7dc75956
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-09-07 18:32:13 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -22,23 +22,25 @@ class Dishdetail extends Component {
             );
         });
         return (
-            <div class="row">
-                <div className="col-12 col-md-5 m-1   ">
-                    <Card >
-                        <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-                        <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1   ">
-                    <Card>
-                        <CardBody>
-                            <CardTitle>{"Comments"}</CardTitle>
-                            <CardText>{comments}</CardText>
-                        </CardBody>
-                    </Card>
+            <div className="container">
+                <div class="row">
+                    <div className="col-12 col-md-5 m-1   ">
+                        <Card >
+                            <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                                <CardTitle>{this.props.dish.name}</CardTitle>
+                                <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1   ">
+                        <Card>
+                            <CardBody>
+                                <CardTitle>{"Comments"}</CardTitle>
+                                <CardText>{comments}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
                 </div>
             </div>
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,38 +4,13 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "re
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        };
     }
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
-    // renderDish(dish) {
-    //     if (dish != null) {
-    //         return (
-    //             <Card>
-    //                 <CardImg width="100%" src={dish.image} alt={dish.name} />
-    //                 <CardBody>
-    //                     <CardTitle>{dish.name}</CardTitle>
-    //                     <CardText>{dish.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
 
-    //         );
-
-    //     }
-    //     else {
-    //         return (
-    //             <div></div>
-    //         );
-    //     }
-    // }
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -50,9 +25,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-
-                <Dishdetail dish={this.state.selectedDish} />
-
             </div>
 
 
