a64922e509d2895c84468cb6810c54acf5763abf
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-09-08 19:25:50 +0530

    Header and Footer added

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -39,7 +39,7 @@ function Footer(props) {
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-auto">
-                        <p>© Copyright 2018 Ristorante Con Fusion</p>
+                        <p>© Copyright 2021 Ristorante Con Fusion</p>
                     </div>
                 </div>
             </div>
