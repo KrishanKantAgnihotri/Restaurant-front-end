5c80bcecdda2cf736b5bf842318fc5d4e64e13d7
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-09-13 15:34:11 +0530

    AboutComponent added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Contact from "./ContactComponent";
 import Menu from "./MenuComponent";
+import About from "./AboutComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
 import { LEADERS } from "../shared/leaders";
@@ -43,6 +44,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => < Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
+                    <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from "react-router-dom";
 
-function RenderMenuItem({ dish, onClick }) {
+function RenderMenuItem({ dish }) {
     return (
         <Card >
             <Link to={`/menu/${dish.id}`}>
